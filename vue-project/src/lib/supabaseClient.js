b2e11590d6af8507c57d9cc3c1777cf04a8d570b
Clarice Benoman <156344601+apccvbenoman@users.noreply.github.com>
2025-02-26 18:35:36 +0800

    supabase client

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://rzykehangfwoenrphyvx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6eWtlaGFuZ2Z3b2VucnBoeXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NjE1MzQsImV4cCI6MjA1NjEzNzUzNH0.E4goeN1MuTc_ERDAqU_i1bmoH7GX3VtHGsDeXIdTGww')
