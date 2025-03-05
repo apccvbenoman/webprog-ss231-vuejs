b6a0e73aacb1a62bb5ff4cb5e2477dfba9efac0f
Clarice Benoman <156344601+apccvbenoman@users.noreply.github.com>
2025-03-05 16:28:37 +0000

    display comments

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,31 +1,13 @@
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-
-  const instruments = ref([])
-
-  async function getInstruments() {
-    const { data } = await supabase.from('instruments').select()
-    instruments.value = data
-  }
-
-  onMounted(() => {
-    getInstruments()
-  })
-  </script>
-
 <template>
 
-<ul>
-      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-    </ul>
-    
     <h1>Food</h1>
+    <instruments/> <br>
     <food-item/> <br>
     <food-item2/> <br>
     <food-item/> <br>
     <food-item2/> <br>
-    <personal-profile/> <br>
+    <comment-form/> <br>
+    <comment/> <br>
 
  </template>
     
